e8f6bb05c96ca863c2d594529b716f818e7b8469
AshutoshSingh2021 <ashutoshsingh15102@gmail.com>
2023-04-07 00:07:10 +0530

    Secured user interaction with bcrypt and jwt secret sign

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "atah:YeSiddhHotaHaiKi_YeUpabhokta.PramadikHai";
 
 // Create a User using: POST "/api/auth/createUser". No login required
 
@@ -33,13 +37,25 @@ router.post(
           .json({ error: "Sorry a user with this email already exists" });
       }
 
+      const salt = await bcrypt.genSalt(10);
+      const securedPassword = await bcrypt.hash(req.body.password, salt);
       // create a new user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: securedPassword,
       });
-      res.json(user);
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const authToken = jwt.sign(data, JWT_SECRET);
+      console.log(authToken);
+
+      res.json({ authToken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Something went wrong!!");
